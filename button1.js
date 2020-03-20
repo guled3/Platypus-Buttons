049557314c231ffb37fbe069343e7a2c5f89a3f0
guled3 <56385581+guled3@users.noreply.github.com>
2020-03-19 18:24:13 -0700

    Update button1.js

--- button1.js
+++ button1.js
@@ -1,15 +1,8 @@
 var content = document.getElementById("content");
 var button = document.getElementById("show-more");
 
-button.onclick = function(){
-
-    if(content.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else {
-        //expand the box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-    }
-} 
+if(content.classList.contains('hidden')){
+    content.className = '';
+} else {
+  content.className = 'hidden'; 
+}; 
